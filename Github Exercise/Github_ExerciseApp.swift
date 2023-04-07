aa4438b8c865a19acdc823f85c70e7d0ae935d1b
ServantofLord <128671036+ServantofLord@users.noreply.github.com>
2023-04-07 21:55:16 +0800

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Lawrence Lau on 7/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
